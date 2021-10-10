10bee3288018254cf905a0fa1c7abb60028e7dc7
Kobe Tran <kobetran@KT-Mac-Mini-2018-Model.local>
2021-10-09 22:59:01 -0700

    scrap react-router-dom for now; proceed later

--- src/App.js
+++ src/App.js
@@ -1,65 +1,64 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import "./Styles/App.css";
 import TopHalfrender from './Cards/topHalf.js';
 import DateStrategy from './Cards/datesCard.js';
 import StudentLoansStrategy from './Cards/studentLoans.js';
 import Esports_Strategy from './Cards/esports.js';
-class App extends Component
-{
-  
-  render(){
-    return(
+class App extends Component {
+
+  render() {
+    return (
       <div>
 
 
-        <div className = "topHalf" >
-          < TopHalfrender/>
+        <div className="topHalf" >
+          <TopHalfrender />
         </div>
 
-        <div className = "bottomHalf">
+        <div className="bottomHalf">
           <h1> Alternative Strategies </h1>
-          <div className = "Alt-Strategies">
-            <div className = "datesCard">
-              <DateStrategy/>
+          <div className="Alt-Strategies">
+            <div className="datesCard">
+              <DateStrategy />
             </div>
-            <div className = "leverageStudentLoans">
-              <StudentLoansStrategy/>
+            <div className="leverageStudentLoans">
+              <StudentLoansStrategy />
             </div>
-            <div className = "esports">
-              <Esports_Strategy/>         
+            <div className="esports">
+              <Esports_Strategy />
             </div>
 
           </div>
         </div>
-        
+
         <h1>Stock Focused</h1>
-        <div className = "Stock-Focused-Strategies">
-            <div className = "datesCard">
-              <DateStrategy/>
-            </div>
-            <div className = "leverageStudentLoans">
-              <StudentLoansStrategy/>
-            </div>
-            <div className = "esports">
-              <Esports_Strategy/>         
-            </div>
+        <div className="Stock-Focused-Strategies">
+          <div className="datesCard">
+            <DateStrategy />
+          </div>
+          <div className="leverageStudentLoans">
+            <StudentLoansStrategy />
+          </div>
+          <div className="esports">
+            <Esports_Strategy />
+          </div>
         </div>
-        
+
         <h1>Cryptocurrency</h1>
-        <div className = "Cryptocurrency-Strategies">
-            <div className = "datesCard">
-              <DateStrategy/>
-            </div>
-            <div className = "leverageStudentLoans">
-              <StudentLoansStrategy/>
-            </div>
-            <div className = "esports">
-              <Esports_Strategy/>         
-            </div>
+        <div className="Cryptocurrency-Strategies">
+          <div className="datesCard">
+            <DateStrategy />
+          </div>
+          <div className="leverageStudentLoans">
+            <StudentLoansStrategy />
+          </div>
+          <div className="esports">
+            <Esports_Strategy />
+          </div>
         </div>
       </div>
 
-      
+
     );
   }
 }
--- src/SharedComponents/header.js
+++ src/SharedComponents/header.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
 //Need this to switch pages
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-} from "react-router-dom";
-import TopHalfrender from '../Cards/topHalf';
-import TheTeamRender from '../Pages/theTeam';
+// import {
+//     BrowserRouter as Router,
+//     Switch,
+//     Route,
+//     Link
+// } from "react-router-dom";
+// import TheTeamRender from '../Pages/theTeam';
 import "../Styles/topHalf.css";
 
 class HeaderRender extends Component {
@@ -15,44 +14,46 @@ class HeaderRender extends Component {
         return (
             //wrap the code in Router
             //Router is used to direct to new pages when buttons are clicked
-            <Router>
-                <header className="header-container">
-                    <nav class="nav-bar">
-                        <div>
-                            {/* Backslash directs to home */}
-                            <Link to="/">
+            // <Router>
+            <header className="header-container">
+                <nav class="nav-bar">
+                    <div>
+                        {/* Backslash directs to home */}
+                        {/* <Link to="/">
                                 <h1>QuantHub</h1>
-                            </Link>
-                        </div>
+                            </Link> */}
+                        <h1>QuantHub</h1>
+                    </div>
 
-                        <div className="nav-menu">
+                    <div className="nav-menu">
 
-                            <button className="buttons" id="nav-button1" onClick={redirect_Team}>
-                                The Team
-                            </button>
-                            <button className="buttons" id="nav-button2" onClick={redirect_About}>
-                                About
-                            </button>
+                        <button className="buttons" id="nav-button1" onClick={redirect_Team}>
+                            The Team
+                        </button>
+                        <button className="buttons" id="nav-button2" onClick={redirect_About}>
+                            About
+                        </button>
 
-                        </div>
-                    </nav>
-                    {/* A <Switch> looks through its children <Route>s and
+                    </div>
+                </nav>
+                {/* A <Switch> looks through its children <Route>s and
                         renders the first one that matches the current URL. */}
-                    <Switch>
+                {/* <Switch>
                         <Route path="/">
                             <Home />
                         </Route>
-                    </Switch>
-                </header>
-            </Router>
+                    </Switch> */}
+                {/* </Router> */}
+            </header>
+
 
         );
     }
 }
 
-function Home() {
-    return <TopHalfrender />;
-}
+// function Home() {
+//     return <TopHalfrender />;
+// }
 
 function redirect_Team() {
     console.log("Team Directed");
